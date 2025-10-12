eca55f2b35ea5b9af67016977b9dbd790a66b64e
dona-dlollin <donadlollin@gmail.com>
2025-10-12 16:30:12 +0700

    belajar upload file dan request format json

---
+++ golang-web/chapter-1.11/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+	"os"
+	"io"
+	"path/filepath"
+)
+
+func main() {
+	 http.HandleFunc("/", routeIndexGet)
+	 http.HandleFunc("/process", routeSubmitPost)
+ 	 fmt.Println("server started at localhost:9000")
+ 	 http.ListenAndServe(":9000", nil)
+
+}
+
+func routeIndexGet(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		var tmpl = template.Must(template.ParseFiles("view.html"))
+		if err := tmpl.Execute(w, nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+
+		}
+		return
+	}
+
+	http.Error(w, "", http.StatusBadRequest)
+
+}
+
+func routeSubmitPost(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	if err := r.ParseMultipartForm(1024); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	alias := r.FormValue("alias")
+
+	uploadedFile, handler, err := r.FormFile("file")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer uploadedFile.Close()
+
+	dir, err := os.Getwd()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	filename := handler.Filename
+	if alias != "" {
+		filename = fmt.Sprintf("%s%s", alias, filepath.Ext(handler.Filename))
+	}
+
+	fileLocation := filepath.Join(dir, "files", filename)
+	targetFile, err := os.OpenFile(fileLocation, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+ 	http.Error(w, err.Error(), http.StatusInternalServerError)
+ 	return
+	}
+
+	defer targetFile.Close()
+
+	if _, err := io.Copy(targetFile, uploadedFile); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+ 		return
+
+	}
+
+	w.Write([]byte("done"))
+
+	http.Error(w, "", http.StatusBadRequest)
+}
---
+++ golang-web/chapter-1.12/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+	"encoding/json"
+)
+
+func main() {
+	 http.HandleFunc("/", handleIndex)
+	 http.HandleFunc("/save", handleSave)
+	 http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+ 	 fmt.Println("server started at localhost:9000")
+ 	 http.ListenAndServe(":9000", nil)
+
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("view.html"))
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func handleSave(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		decoder := json.NewDecoder(r.Body)
+
+
+		payload := struct {
+			Name string `json:"name"`
+			Age int `json:"age"`
+			Gender string `json:"gender"`
+		}{}
+
+		if err := decoder.Decode(&payload); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+			fmt.Println(payload)
+		}
+
+		message := fmt.Sprintf(
+			"hello, my name is %s. I'm %d year old %s",
+			payload.Name,
+			payload.Age,
+			payload.Gender,
+		)
+
+		w.Write([]byte(message))
+		return
+	}
+	 http.Error(w, "Only accept POST request", http.StatusBadRequest)
+
+}
